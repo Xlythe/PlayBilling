b24bbf247bbba279e883721b633714137af8eece
Will <xlythe@gmail.com>
2018-09-19 21:35:23 -0700

    Finalize new billing lib support

--- play-billing/src/main/java/com/android/vending/billing/util/PurchaseActivity.java
+++ play-billing/src/main/java/com/android/vending/billing/util/PurchaseActivity.java
@@ -1,115 +1,125 @@
 package com.android.vending.billing.util;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingClientStateListener;
 import com.android.billingclient.api.BillingFlowParams;
 import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchaseHistoryResponseListener;
 import com.android.billingclient.api.PurchasesUpdatedListener;
 
 import java.util.List;
+import java.util.Locale;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class PurchaseActivity extends AppCompatActivity {
+    private static final String TAG = "PlayBilling";
 
+    // Allows us to talk to the Play Store app on the device, and query it for in app purchase and subscription
+    // information.
     private BillingClient mBillingClient;
 
+    // Fires whenever we connect (or disconnect) from the Play Store. While we're connected, we can
+    // query for purchase history or purchase new items.
+    private final BillingClientStateListener mBillingClientStateListener = new BillingClientStateListener() {
+        @Override
+        public void onBillingSetupFinished(@BillingClient.BillingResponse int billingResponseCode) {
+            if (billingResponseCode == BillingClient.BillingResponse.OK) {
+                Log.d(TAG, "Billing successfully setup");
+                onIabSetupSuccess();
+                mBillingClient.queryPurchaseHistoryAsync(BillingClient.SkuType.INAPP, mPurchaseHistoryResponseListener);
+            } else {
+                Log.w(TAG, "Billing successfully setup, but received error " + PurchaseActivity.toString(billingResponseCode));
+                onIabSetupFailure();
+            }
+        }
+
+        @Override
+        public void onBillingServiceDisconnected() {
+            Log.w(TAG, "Billing temporarily disconnected");
+            onIabSetupFailure();
+        }
+    };
+
+    private final PurchaseHistoryResponseListener mPurchaseHistoryResponseListener = new PurchaseHistoryResponseListener() {
+        @Override
+        public void onPurchaseHistoryResponse(@BillingClient.BillingResponse int billingResponseCode, List<Purchase> purchases) {
+            if (billingResponseCode == BillingClient.BillingResponse.OK) {
+                // Ask the app for all in app purchases they expect the user can buy.
+                List<String> expectedPurchases = getSkus();
+
+                // Ask the play store for the user's purchase history, and notify the app.
+                // This way, if the user bought a new device, the app can re-adjust its state.
+                for (Purchase purchase : purchases) {
+                    onPurchaseSuccess(purchase.getSku(), purchase);
+                    expectedPurchases.remove(purchase.getSku());
+                }
+
+                // For everything the play store did not have a purchase history for, report it as such.
+                // This way, if the purchase was refunded, the app can re-adjust its state.
+                for (String sku : expectedPurchases) {
+                    onPurchaseFailed(sku);
+                }
+            } else {
+                Log.w(TAG, "Attempted to query purchase history, but received error: " + PurchaseActivity.toString(billingResponseCode));
+            }
+        }
+    };
+
+    // Fires whenever a purchase has been made, with the status of the purchase.
+    private final PurchasesUpdatedListener mPurchasesUpdatedListener = new PurchasesUpdatedListener() {
+        @Override
+        public void onPurchasesUpdated(@BillingClient.BillingResponse int billingResponseCode, List<Purchase> purchases) {
+            if (billingResponseCode == BillingClient.BillingResponse.OK) {
+                for (Purchase purchase : purchases) {
+                    onPurchaseSuccess(purchase.getSku(), purchase);
+                }
+            } else {
+                Log.w(TAG, "Attempted to purchase an item, but received error: " + PurchaseActivity.toString(billingResponseCode));
+            }
+        }
+    };
+
     /**
-     * Return your key from the Dev Console
-     * */
+     * Return your key from the Dev Console.
+     */
     protected abstract String getKey();
 
     /**
-     * Return all the skus you want to verify purchases for
-     * */
+     * Return all the skus you want to verify purchases for.
+     */
     protected abstract List<String> getSkus();
 
-    protected void onSkuFound(String sku) {}
+    /**
+     * Successfully connected to the Play Store. You can purchase items now.
+     */
+    protected void onIabSetupSuccess() {}
 
-    protected void onSkuLost(String sku) {}
+    /**
+     * Failed to connect to the Play Store. Disable options to purchase items.
+     */
+    protected void onIabSetupFailure() {}
+
+    /**
+     * An in app purchase has been confirmed.
+     */
+    protected void onPurchaseSuccess(String sku, Purchase info) {}
+
+    /**
+     * An in app purchase was not found.
+     */
+    protected void onPurchaseFailed(String sku) {}
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setResult(RESULT_CANCELED);
-
-        mBillingClient = BillingClient.newBuilder(this).setListener(new PurchasesUpdatedListener() {
-            @Override
-            public void onPurchasesUpdated(@BillingClient.BillingResponse int responseCode, @Nullable List<Purchase> purchases) {
-                String sku = null; // TODO
-                Purchase purchase = null; // TODO
-                switch (responseCode) {
-                    case BillingClient.BillingResponse.FEATURE_NOT_SUPPORTED:
-                    case BillingClient.BillingResponse.SERVICE_DISCONNECTED:
-                    case BillingClient.BillingResponse.USER_CANCELED:
-                    case BillingClient.BillingResponse.SERVICE_UNAVAILABLE:
-                    case BillingClient.BillingResponse.BILLING_UNAVAILABLE:
-                    case BillingClient.BillingResponse.ITEM_UNAVAILABLE:
-                    case BillingClient.BillingResponse.DEVELOPER_ERROR:
-                    case BillingClient.BillingResponse.ERROR:
-                    case BillingClient.BillingResponse.ITEM_ALREADY_OWNED:
-                    case BillingClient.BillingResponse.ITEM_NOT_OWNED:
-                        onPurchaseFailed(sku);
-                        break;
-                    case BillingClient.BillingResponse.OK:
-                        onSkuFound(sku);
-                        onPurchaseSuccess(sku, purchase);
-                        break;
-                }
-            }
-        }).build();
-        mBillingClient.startConnection(new BillingClientStateListener() {
-            @Override
-            public void onBillingSetupFinished(@BillingClient.BillingResponse int billingResponseCode) {
-                if (billingResponseCode == BillingClient.BillingResponse.OK) {
-                    // The billing client is ready. You can query purchases here.
-                }
-            }
-            @Override
-            public void onBillingServiceDisconnected() {
-                // Try to restart the connection on the next request to
-                // Google Play by calling the startConnection() method.
-            }
-        });
+        mBillingClient = BillingClient.newBuilder(this).setListener(mPurchasesUpdatedListener).build();
+        mBillingClient.startConnection(mBillingClientStateListener);
     }
-
-    @Override
-    public void onIabSetupFinished(IabResult result) {
-        if (result.isSuccess()) {
-            iabSetupSuccess = true;
-            onIabSetupSuccess();
-        } else {
-            onIabSetupFailure();
-        }
-
-        // Check to see if the item has already been purchased
-        IabHelper.QueryInventoryFinishedListener gotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-            @Override
-            public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
-                if(inventory != null) {
-                    for (String sku : getSkus()) {
-                        skuFound(sku, inventory.getAllOwnedSkus().contains(sku));
-                    }
-                }
-            }
-        };
-        try {
-            billingHelper.queryInventoryAsync(true, getSkus(), gotInventoryListener);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void onIabSetupSuccess() {}
-
-    protected void onIabSetupFailure() {}
-
-    protected void onPurchaseSuccess(String sku, Purchase info) {}
-
-    protected void onPurchaseFailed(String sku) {}
 
     public void purchaseItem(String sku) {
         purchaseItem(sku, BillingClient.SkuType.INAPP);
@@ -126,5 +136,34 @@ public abstract class PurchaseActivity extends AppCompatActivity {
     protected void onDestroy() {
         mBillingClient.endConnection();
         super.onDestroy();
+    }
+
+    private static String toString(@BillingClient.BillingResponse int billingResponseCode) {
+        switch (billingResponseCode) {
+            case BillingClient.BillingResponse.FEATURE_NOT_SUPPORTED:
+                return String.format(Locale.US, "[%d]FEATURE_NOT_SUPPORTED", billingResponseCode);
+            case BillingClient.BillingResponse.SERVICE_DISCONNECTED:
+                return String.format(Locale.US, "[%d]SERVICE_DISCONNECTED", billingResponseCode);
+            case BillingClient.BillingResponse.USER_CANCELED:
+                return String.format(Locale.US, "[%d]USER_CANCELED", billingResponseCode);
+            case BillingClient.BillingResponse.SERVICE_UNAVAILABLE:
+                return String.format(Locale.US, "[%d]SERVICE_UNAVAILABLE", billingResponseCode);
+            case BillingClient.BillingResponse.BILLING_UNAVAILABLE:
+                return String.format(Locale.US, "[%d]BILLING_UNAVAILABLE", billingResponseCode);
+            case BillingClient.BillingResponse.ITEM_UNAVAILABLE:
+                return String.format(Locale.US, "[%d]ITEM_UNAVAILABLE", billingResponseCode);
+            case BillingClient.BillingResponse.DEVELOPER_ERROR:
+                return String.format(Locale.US, "[%d]DEVELOPER_ERROR", billingResponseCode);
+            case BillingClient.BillingResponse.ERROR:
+                return String.format(Locale.US, "[%d]ERROR", billingResponseCode);
+            case BillingClient.BillingResponse.ITEM_ALREADY_OWNED:
+                return String.format(Locale.US, "[%d]ITEM_ALREADY_OWNED", billingResponseCode);
+            case BillingClient.BillingResponse.ITEM_NOT_OWNED:
+                return String.format(Locale.US, "[%d]ITEM_NOT_OWNED", billingResponseCode);
+            case BillingClient.BillingResponse.OK:
+                return String.format(Locale.US, "[%d]OK", billingResponseCode);
+            default:
+                return String.format(Locale.US, "[%d]UNKNOWN", billingResponseCode);
+        }
     }
 }
